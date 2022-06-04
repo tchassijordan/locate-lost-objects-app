2f7b106179ee46f5fa5d69d01cdf9d98cbb8c1a1
tchassijordan <tchassijordan@gmail.com>
2022-06-04 04:11:03 +0100

    feat(footer): add syles to footer

--- locate-frontend/src/components/Button.tsx
+++ locate-frontend/src/components/Button.tsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
-export default function Button({ Icon, placeholder, type }: TProps) {
+export default function Button({
+  Icon,
+  placeholder,
+  type,
+  classes,
+  primary,
+  secondary,
+  to
+}: TProps) {
+  let style: string;
+
+  if (primary)
+    style =
+      'group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 transition-all';
+  else if (secondary) style = '';
+  else style = '';
+
   return (
     <button
       type={type}
-      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 transition"
-    >
+      className={cn(classes , style)}>
       {Icon && (
-        <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+        <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
           {Icon}
         </span>
       )}
-      {placeholder}
+      {to ? <Link to={to}>{placeholder}</Link> : placeholder}
     </button>
   );
 }
 
 type TProps = {
-  Icon: JSX.Element,
-  placeholder: string,
-  type: React.ButtonHTMLAttributes<HTMLButtonElement>["type"],
-}
+  Icon?: JSX.Element;
+  placeholder: string;
+  type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  classes?: string;
+  primary?: boolean;
+  secondary?: boolean;
+  to?: string;
+};
