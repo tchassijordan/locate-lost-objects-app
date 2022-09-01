96a64ecd44e75e8cb88a56dc7cfbc27ab324f647
tchassijordan <tchassijordan@gmail.com>
2022-09-01 17:07:07 +0100

    feat(component): extend InputField to displays error msgs

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -6,29 +6,37 @@ export default function InputField({
   onBlur,
   value,
   name,
-  type
+  type,
+  hasError,
+  errorMsg
 }: Prop) {
   const labelName = name.replaceAll('_', ' ');
   return (
     <div>
-      <label
-        htmlFor={name}
-        className='block text-sm font-medium capitalize text-gray-700'>
-        {labelName}
-      </label>
-
-      <input
-        type={type}
-        name={name}
-        id={name}
-        autoComplete={name}
-        className={classNames(
-          'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm'
-        )}
-        onChange={onChange}
-        onBlur={onBlur}
-        value={value}
-      />
+      <div>
+        <label
+          htmlFor={name}
+          className='block text-sm font-medium capitalize text-gray-700'>
+          {labelName}
+        </label>
+        <input
+          type={type}
+          name={name}
+          id={name}
+          autoComplete={name}
+          className={classNames(
+            'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm'
+          )}
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value}
+        />
+      </div>
+      {hasError ? (
+        <p className='text-xs tracking-wide text-red-600 sm:text-sm'>
+          {errorMsg}
+        </p>
+      ) : null}
     </div>
   );
 }
@@ -39,4 +47,6 @@ type Prop = {
   name: string;
   type: string;
   value: string;
+  hasError?: boolean;
+  errorMsg?: string;
 };
